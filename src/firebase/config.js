9447c9ca26cb70802543bf7b0223744f76a6a2ed
omerfikri <omer.fikri23@gmail.com>
2024-02-13 22:53:03 +0300

    Created project's template

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, serverTimestamp } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyltMd-zBdBT1-sQKv42DGf7a-sW2tyOI",
+  authDomain: "blog-ee06b.firebaseapp.com",
+  projectId: "blog-ee06b",
+  storageBucket: "blog-ee06b.appspot.com",
+  messagingSenderId: "117542492649",
+  appId: "1:117542492649:web:89cf333456387680651378",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth();
+const timestamp = serverTimestamp();
+const storageRef = getStorage();
+
+export { db, timestamp, auth, storageRef };
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { auth } from "./firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+
+let app;
+
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App);
+
+    app.use(router);
+
+    app.mount("#app");
+  }
+});
